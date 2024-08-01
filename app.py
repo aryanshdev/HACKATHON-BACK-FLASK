8871a1d90b6fef598332d31906425ec582b57f07
AryanshDev <aryanshappmaker@gmail.com>
2024-08-01 17:01:29 +0530

    FIXED FILE UPLOAD

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, session, request
+import flask
 from flask_session import Session 
+import os
 
 # Custom module imports (assuming they are custom modules)
 import EXCEL_MANIPULATION
 import CSV_MANIPULATION 
 
-# Initialize the Flask app
-app = Flask(__name__)
 
-# Configure the session to use the filesystem
-app.config['SESSION_TYPE'] = 'filesystem' 
+app = flask.Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = 'AVENGEAI-FLASK-SESSION'
+Session(app)
 
-# Initialize the session
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Initialize the flask.session
 Session(app)
 
 # Define routes and logic here
@@ -21,25 +24,22 @@ def index():
     return 'Hello, World!'
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    return 'Prediction'
-
 @app.route('/uploadFile', methods=['POST'])
-def uploadFile():
-    file = request.files['file']
-    if(file.filename.endswith('.xlsx') ): # Check if the file is an excel file
-        openedFile =  EXCEL_MANIPULATION.EXCEL_MANIPULATION(file)
-        return ({'message': 'File uploaded successfully!' , "data" : openedFile.getData()})
-    elif(file.filename.endswith('.csv')):
-        openedFile =  CSV_MANIPULATION.CSV_MANIPULATION(file)
-        return ({'message': 'File uploaded successfully!' , "data" : openedFile.getData()})
-    else:
-        return ({'message': 'Invalid file format!'}), 400
+def upload_file():
+    file = flask.request.files['uploadFile']
+    if(file.filename.endswith('.csv')):
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        flask.session["current"] = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        return CSV_MANIPULATION.CSV_MANIPULATION(flask.session["current"]).getData();
+    elif(file.filename.endswith('.xlsx')):
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        flask.session["current"] = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        return EXCEL_MANIPULATION.EXCEL_MANIPULATION(flask.session["current"]).getData();
+    
 
 @app.route('/deleteCol', methods=['POST'])
 def deleteCol():
-    if(session['current']):
-        return session['current'].clearCol(request.form['col'])
+    if(flask.session['current']):
+        return flask.session['current'].clearCol(flask.request.form['col'])
     else:
         return ({'message': 'No file uploaded yet!'})

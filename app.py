008556f1fa101f4a1f5ef3ce69c38f0ef190c2bf
AryanshDev <aryanshappmaker@gmail.com>
2024-08-01 23:15:12 +0530

    Added Methods

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 from flask_session import Session 
 import os
+from flask_cors import CORS
 
 # Custom module imports (assuming they are custom modules)
 import EXCEL_MANIPULATION
@@ -15,7 +16,8 @@ Session(app)
 UPLOAD_FOLDER = 'uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Initialize the flask.session
+CORS(app, origins=['http://localhost:5173'])  
+# Initialize session and Allow App to use it
 Session(app)
 
 # Define routes and logic here
@@ -27,14 +29,15 @@ def index():
 @app.route('/uploadFile', methods=['POST'])
 def upload_file():
     file = flask.request.files['uploadFile']
+    print(file.filename)
     if(file.filename.endswith('.csv')):
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
         flask.session["current"] = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        return CSV_MANIPULATION.CSV_MANIPULATION(flask.session["current"]).getData();
+        return CSV_MANIPULATION.CSV_MANIPULATION(flask.session["current"]).getData(),200;
     elif(file.filename.endswith('.xlsx')):
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
         flask.session["current"] = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        return EXCEL_MANIPULATION.EXCEL_MANIPULATION(flask.session["current"]).getData();
+        return EXCEL_MANIPULATION.EXCEL_MANIPULATION(flask.session["current"]).getData(),200;
     
 
 @app.route('/deleteCol', methods=['POST'])
@@ -44,3 +47,13 @@ def deleteCol():
     else:
         return ({'message': 'No file uploaded yet!'})
 
+@app.route('/deleteFile', methods=['DELETE'])
+def deleteFile():
+    print(flask.session)
+    if(flask.session.get('current')):
+        os.remove(flask.session.get('current'))
+        flask.session.pop('current', None)
+        return ({'message': 'File deleted successfully!'})
+
+    else:
+        return ({'message': 'No file uploaded yet!'})
